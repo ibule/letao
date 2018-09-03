f14c1e4a347fdbb7619abc85f0cb8288bdfa9a4a
ibule <taotaohu3.14@qq.com>
2018-09-03 21:47:07 +0800

    乐淘电商项第一天实现首页和框架的搭建

--- public/m/js/index.js
+++ public/m/js/index.js
@@ -1,6 +1,7 @@
 $(function(){
 var letao=new Letao();
-letao.slider()  
+letao.slider();
+letao.scroll();
 })
 var Letao=function(){};
 Letao.prototype={
@@ -9,5 +10,10 @@ Letao.prototype={
         gallery.slider({
           interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
         });      
+    },
+    scroll:function(){
+        mui('.mui-scroll-wrapper').scroll({
+            deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        });  
     }
 }     
